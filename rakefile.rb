29d753dc93599647fdccf445574de3265a6a5f58
Oskar Gewalli <oskar@gewalli.se>
2013-10-17 18:12:14 +0200

    Nuget package

---
+++ rakefile.rb
@@ -0,0 +1,30 @@
+require 'albacore'
+
+dir = File.dirname(__FILE__)
+
+desc "build using msbuild"
+msbuild :build do |msb|
+  msb.properties :configuration => :Debug
+  msb.targets :Clean, :Rebuild
+  msb.verbosity = 'quiet'
+  msb.solution =File.join(dir, "src", "With.sln")
+end
+
+task :core_copy_to_nuspec => [:build] do
+  output_directory_lib = File.join(dir,"nuget/lib/40/")
+  mkdir_p output_directory_lib
+  ['With'].each{ |project|
+    cp Dir.glob("./src/#{project}/bin/Debug/*.dll"), output_directory_lib
+  }
+  
+end
+
+desc "create the nuget package"
+task :nugetpack => [:core_nugetpack]
+
+task :core_nugetpack => [:core_copy_to_nuspec] do |nuget|
+  cd File.join(dir,"nuget") do
+    sh "..\\src\\.nuget\\NuGet.exe pack With.nuspec"
+  end
+end
+
