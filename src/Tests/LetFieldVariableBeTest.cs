bbba016ca7b432a33e5eea25a2027a23f20f4d57
Oskar Gewalli <oskar@gewalli.se>
2014-11-02 11:05:48 +0100

    Added tests of property access

---
+++ src/Tests/LetFieldVariableBeTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using NUnit.Framework;
+using With;
+
+namespace Tests
+{
+	[TestFixture]
+	public class LetFieldVariableBeTest
+	{
+		class MyClass
+		{
+			public int Value = 100;
+			public static int StaticField = 1000;
+		}
+	
+		[Test]
+		public void Test()
+		{
+			var myClass = new MyClass();
+			using (Let.Of(myClass)
+					  .Property(obj=>obj.Value)
+					  .Be(13))
+			{
+				Assert.That(myClass.Value, Is.EqualTo(13));
+			}
+			Assert.That(myClass.Value, Is.EqualTo(100));
+		}
+
+		class ClassWithClass
+		{
+			public MyClass value = new MyClass();
+		}
+
+		[Test]
+		public void Static()
+		{
+			using (Let.Of(()=> MyClass.StaticField)
+					  .Be(13))
+			{
+				Assert.That(MyClass.StaticField, Is.EqualTo(13));
+			}
+			Assert.That(MyClass.StaticField, Is.EqualTo(1000));
+		}
+
+		[Test]
+		public void Test_instance()
+		{
+			var myClass = new MyClass();
+			using (Let.Of(()=>myClass.Value)
+					  .Be(13))
+			{
+				Assert.That(myClass.Value, Is.EqualTo(13));
+			}
+			Assert.That(myClass.Value, Is.EqualTo(100));
+		}
+	}
+}
+
---
+++ src/Tests/LetPropertyVariableBeTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using NUnit.Framework;
+using With;
+
+namespace Tests
+{
+
+	[TestFixture]
+	public class LetPropertyVariableBeTest
+	{
+		class MyClass
+		{
+			public MyClass ()
+			{
+				Value = 100;
+			}
+			public int Value{ get; set;}
+			private static int _staticValue=1000;
+			public static int StaticProperty {
+				get{ return _staticValue;}
+				set{ _staticValue = value;}
+			}
+		}
+
+		[Test]
+		public void Test()
+		{
+			var myClass = new MyClass();
+			using (Let.Of(myClass)
+				.Property(obj=>obj.Value)
+				.Be(13))
+			{
+				Assert.That(myClass.Value, Is.EqualTo(13));
+			}
+			Assert.That(myClass.Value, Is.EqualTo(100));
+		}
+
+		class ClassWithClass
+		{
+			public MyClass value = new MyClass();
+		}
+
+		[Test]
+		public void Static()
+		{
+			using (Let.Of(()=> MyClass.StaticProperty)
+				.Be(13))
+			{
+				Assert.That(MyClass.StaticProperty, Is.EqualTo(13));
+			}
+			Assert.That(MyClass.StaticProperty, Is.EqualTo(1000));
+		}
+
+		[Test]
+		public void Test_instance()
+		{
+			var myClass = new MyClass();
+			using (Let.Of(()=>myClass.Value)
+				.Be(13))
+			{
+				Assert.That(myClass.Value, Is.EqualTo(13));
+			}
+			Assert.That(myClass.Value, Is.EqualTo(100));
+		}
+	}
+}
+
