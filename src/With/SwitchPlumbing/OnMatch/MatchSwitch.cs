9934502bc877c45d938bb559279bc61cd75e673d
Oskar Gewalli <oskar@gewalli.se>
2014-11-04 21:24:31 +0100

    Separate files

--- src/With/SwitchPlumbing/OnMatch/MatchSwitch.cs
+++ src/With/SwitchPlumbing/OnMatch/MatchSwitch.cs
@@ -19,31 +19,5 @@ namespace With.SwitchPlumbing
 			throw new NotImplementedException ();
 		}
 	}
-
-	public class MatchSwitch<Ingoing,Outgoing>:MatchSwitch<Ingoing>{
-		public MatchSwitch<Ingoing,Outgoing> Case (Ingoing expected, Func<Outgoing> result)
-		{
-			throw new NotImplementedException ();
-		}
-
-		public MatchSwitch<Ingoing,Outgoing> Case (IEnumerable<Ingoing> expected, Func<Ingoing,Outgoing> result)
-		{
-			throw new NotImplementedException ();
-		}
-
-		public MatchSwitch<Ingoing,Outgoing> Else (Func<Ingoing,Outgoing> result)
-		{
-			throw new NotImplementedException ();
-		}
-		public Outgoing Value()
-		{
-			throw new NotImplementedException ();
-		}
-
-		public static implicit operator Outgoing(MatchSwitch<Ingoing,Outgoing> d)
-		{
-			return d.Value ();
-		}
-
-	}
+	
 }
