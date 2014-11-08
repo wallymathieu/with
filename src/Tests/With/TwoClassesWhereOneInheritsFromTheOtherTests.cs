105ce1bfd35e3fc3c2768cc9b32baf2323a4b371
Oskar Gewalli <gewalli@gmail.com>
2014-11-08 11:15:30 +0100

    Changed parameter order in order to conform to xunit

--- src/Tests/With/TwoClassesWhereOneInheritsFromTheOtherTests.cs
+++ src/Tests/With/TwoClassesWhereOneInheritsFromTheOtherTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Xunit;
+using TestAttribute = Xunit.FactAttribute;
 using With;
 using Ploeh.AutoFixture.Xunit;
 using Xunit.Extensions;
@@ -22,30 +23,34 @@ namespace Tests
 
         public class MyClass2 : MyClass
         {
-            public DateTime MyProperty3 { get; set; }
+            private readonly DateTime myProperty3;
             public MyClass2(int myProperty, string myProperty2, DateTime myProperty3)
                 : base(myProperty, myProperty2)
             {
-                MyProperty3 = myProperty3;
+                this.myProperty3 = myProperty3;
             }
+            public DateTime MyProperty3 { get { return myProperty3; } private set { throw new Exception(); } }
+
         }
 
         [Theory, AutoData]
         public void A_class_should_map_its_parents_properties(MyClass myClass, DateTime time)
         {
             var ret = myClass.As<MyClass2>(time);
-            Assert.Equal(ret.MyProperty, myClass.MyProperty);
-            Assert.Equal(ret.MyProperty2, myClass.MyProperty2);
-            Assert.Equal(ret.MyProperty3, time);
+            Assert.Equal(time, ret.MyProperty3);
+
+            Assert.Equal(myClass.MyProperty, ret.MyProperty);
+            Assert.Equal(myClass.MyProperty2, ret.MyProperty2);
         }
 
         [Theory, AutoData]
         public void A_class_should_be_able_to_use_lambda(MyClass myClass, DateTime time)
         {
             var ret = myClass.As<MyClass2>(m => m.MyProperty3 == time);
-            Assert.Equal(ret.MyProperty, myClass.MyProperty);
-            Assert.Equal(ret.MyProperty2, myClass.MyProperty2);
             Assert.Equal(ret.MyProperty3, time);
+
+            Assert.Equal(myClass.MyProperty, ret.MyProperty);
+            Assert.Equal(myClass.MyProperty2, ret.MyProperty2);
         }
 
         [Theory, AutoData]
@@ -53,10 +58,10 @@ namespace Tests
         {
             Object _time = (Object)time;
             var ret = myClass.As<MyClass2>(m => m.MyProperty3 == (DateTime)_time);
-            Assert.Equal(ret.MyProperty, myClass.MyProperty);
-            Assert.Equal(ret.MyProperty2, myClass.MyProperty2);
             Assert.Equal(ret.MyProperty3, time);
-        }
 
+            Assert.Equal(myClass.MyProperty, ret.MyProperty);
+            Assert.Equal(myClass.MyProperty2, ret.MyProperty2);
+        }
     }
 }
