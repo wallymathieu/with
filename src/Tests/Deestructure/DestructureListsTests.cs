08e557075a967f3bbc20256bf7bc28e08a0ca05c
Oskar Gewalli <gewalli@gmail.com>
2014-12-17 22:26:10 +0100

    Destructure list

---
+++ src/Tests/Deestructure/DestructureListsTests.cs
@@ -0,0 +1,25 @@
+﻿using Ploeh.AutoFixture.Xunit;
+using System;
+using Xunit;
+using Xunit.Extensions;
+using With.Destructure;
+
+namespace Tests.Deestructure
+{
+    public class DestructureListsTests
+    {
+        [Theory, AutoData]
+        public void First_variable(
+            int a, int b, int c)
+        {
+            Assert.Equal(a, new[] { a, b, c }.Let((x, _) => x));
+        }
+
+        [Theory, AutoData]
+        public void Second_variable(
+            int a, int b, int c)
+        {
+            Assert.Equal(b, new[] { a, b, c }.Let((x, y, _) => y));
+        }
+    }
+}
---
+++ src/With/Destructure/ListExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace With.Destructure
+{
+    public static class ListExtensions
+    {
+        public static void Let<T>(
+this IEnumerable<T> that, Action<T, IEnumerable<T>> action)
+        {
+            action(that.First(), that.Skip(1));
+        }
+        public static TRet Let<T, TRet>(
+this IEnumerable<T> that, Func<T, IEnumerable<T>, TRet> action)
+        {
+            return action(that.First(), that.Skip(1));
+        }
+
+
+        public static void Let<T>(
+this IEnumerable<T> that, Action<T, T, IEnumerable<T>> action)
+        {
+            action(that.First(), that.Skip(1).First(), that.Skip(2));
+        }
+        public static TRet Let<T, TRet>(
+this IEnumerable<T> that, Func<T, T, IEnumerable<T>, TRet> action)
+        {
+            return action(that.First(), that.Skip(1).First(), that.Skip(2));
+        }
+
+
+    }
+}
