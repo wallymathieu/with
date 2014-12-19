a1cc8e9e19c6f2dab0d18b737042b26fb34dd152
Oskar Gewalli <gewalli@gmail.com>
2014-12-19 21:37:27 +0100

    Added properties tests

--- src/Tests/Destructure/DestructureClassTests.cs
+++ src/Tests/Destructure/DestructureClassTests.cs
@@ -24,6 +24,9 @@ namespace Tests.Destructure
             public readonly string A;
             public readonly string B;
             public readonly string C;
+            public string Af { get { return A; } }
+            public string Bf { get { return B; } }
+            public string Cf { get { return C; } }
         }
 
         public class MyClass1
@@ -37,6 +40,9 @@ namespace Tests.Destructure
             public readonly string A;
             public readonly int B;
             public readonly string C;
+            public string Af { get { return A; } }
+            public int Bf { get { return B; } }
+            public string Cf { get { return C; } }
         }
 
         public class MyClass2
@@ -50,12 +56,15 @@ namespace Tests.Destructure
             public readonly string A;
             public readonly int B;
             public readonly int C;
+            public string Af { get { return A; } }
+            public int Bf { get { return B; } }
+            public int Cf { get { return C; } }
         }
-        private static int Match(object instance)
+
+        private static int MatchFields(object instance)
         {
             return Switch.Match<object, int>(instance).Fields(
                f => f
-                    .IncludeAll()
                     .Case((string a) => -1)
                     .Case((string a, string b, string c) => 1)
                     .Case((string a, int b) => 20)
@@ -65,29 +74,41 @@ namespace Tests.Destructure
                     .Case((string a, int b, int c) => 4)
                   );
         }
-
+        private static int MatchProperties(object instance)
+        {
+            return Switch.Match<object, int>(instance).Properties(
+               f => f
+                    .Case((string a) => -1)
+                    .Case((string a, string b, string c) => 1)
+                    .Case((string a, int b) => 20)
+                    .Case((string a, int b, string c, string d) => 50)
+                    .Case((string a, int b, string c) => 2)
+                    .Case((string a, string b, int c) => 3)
+                    .Case((string a, int b, int c) => 4)
+                  );
+        }
         [Theory, AutoData]
         public void Should_match_the_first_case(
             MyClass0 instance)
         {
-            int result = Match(instance);
-            Assert.Equal(1, result);
+            Assert.Equal(1, MatchFields(instance));
+            Assert.Equal(1, MatchProperties(instance));
         }
 
         [Theory, AutoData]
         public void Should_match_the_second_case(
             MyClass1 instance)
         {
-            int result = Match(instance);
-            Assert.Equal(2, result);
+            Assert.Equal(2, MatchFields(instance));
+            Assert.Equal(2, MatchProperties(instance));
         }
 
         [Theory, AutoData]
         public void Should_match_the_forth_case(
             MyClass2 instance)
         {
-            int result = Match(instance);
-            Assert.Equal(4, result);
+            Assert.Equal(4, MatchFields(instance));
+            Assert.Equal(4, MatchProperties(instance));
         }
     }
 }
