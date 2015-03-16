d2e6eff9c9f6d96a545088f83a489bcce91a35b5
Oskar Gewalli <oskar@gewalli.se>
2015-03-16 07:21:41 +0100

    Should add documentation

--- rakefile.rb
+++ rakefile.rb
@@ -4,18 +4,25 @@ require 'nuget_helper'
 dir = File.dirname(__FILE__)
 
 desc "build using msbuild"
-build :build => [:install_packages] do |msb|
+build :build do |msb|
   msb.prop :configuration, :Debug
   msb.target = [:Rebuild]
   msb.logging = 'minimal'
   msb.sln =File.join(dir, "src", "With.sln")
 end
 
-task :core_copy_to_nuspec => [:build] do
+build :build_release do |msb|
+  msb.prop :configuration, :Release
+  msb.target = [:Rebuild]
+  msb.logging = 'minimal'
+  msb.sln =File.join(dir, "src", "With.sln")
+end
+
+task :core_copy_to_nuspec => [:build_release] do
   output_directory_lib = File.join(dir,"nuget/lib/40/")
   mkdir_p output_directory_lib
   ['With'].each{ |project|
-    cp Dir.glob("./src/#{project}/bin/Debug/*.dll"), output_directory_lib
+    cp Dir.glob("./src/#{project}/bin/Release/With.*"), output_directory_lib
   }
   
 end
