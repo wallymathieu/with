9965d7b69d0b21ac16f3a1b7171a18412b758be9
Oskar Gewalli <gewalli@gmail.com>
2015-09-25 09:56:57 +0200

    HashSet

---
+++ src/With/HashSet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace With
+{
+    public class HashSet
+    {
+        public static HashSet<T> Create<T>(IEnumerable<T> collectionx)
+        {
+            return new HashSet<T>(collectionx);
+        }
+
+        public static HashSet<T> Create<T>(IEnumerable<T> collection, IEqualityComparer<T> comparer)
+        {
+            return new HashSet<T>(collection, comparer);
+        }
+    }
+}
