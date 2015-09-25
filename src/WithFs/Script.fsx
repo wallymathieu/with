6331f3d075545614727a9c6148c2f5e1248b8c89
Oskar Gewalli <oskar@gewalli.se>
2015-09-25 22:24:17 +0200

    WithFs - to prototype things

---
+++ src/WithFs/Script.fsx
@@ -0,0 +1,17 @@
+﻿// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
+// for more guidance on F# programming.
+#I "../Tests/bin/Debug/"
+#r "With.dll"
+
+#load "With.fs"
+open With
+
+let r = [0;1;2;3]
+
+stitch_and_end r -1
+
+let r' = List.map Some r
+
+stitch r'
+stitch_and_end r' None
+
