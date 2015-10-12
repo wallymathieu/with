1db7f6cc09309fb4c50a5b638468517232d9a8f5
Oskar Gewalli <oskar@gewalli.se>
2015-10-12 19:19:09 +0200

    Removed the need for markdownlog (since timings are done so few times)

--- src/Timing/Program.cs
+++ src/Timing/Program.cs
@@ -2,10 +2,26 @@
 using With;
 using System.Linq;
 using System.Collections.Generic;
+#if MARKDOWNLOG
 using MarkdownLog;
-
+#endif
 namespace Timing
 {
+    #if! MARKDOWNLOG
+    using System.Reflection;
+    static class Mardowns{
+        public static string ToMarkdownTable<T>(this IEnumerable<T> self){
+            var properties = typeof(T).GetProperties()
+                .Where(p=>p.CanRead && p.MemberType == MemberTypes.Property)
+                .ToArray();
+            var header = string.Join("|\t", properties.Select(p => p.Name).ToArray());
+            return header + "\n" + string.Join("\n", self.Select(v => MapProperties(properties, v)));
+        }
+        private static string MapProperties(PropertyInfo[] properties,Object v){
+            return String.Join("|\t", properties.Select(p=>p.GetValue(v,null)));
+        }
+    } 
+    #endif
 
     class MainClass
     {
