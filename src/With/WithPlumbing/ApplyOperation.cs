fc992389ec4337184425aa1a4d785f09d604e8e2
Oskar Gewalli <oskar@gewalli.se>
2016-09-29 06:56:22 +0200

    Removed DictionaryAdapter

--- src/With/WithPlumbing/ApplyOperation.cs
+++ src/With/WithPlumbing/ApplyOperation.cs
@@ -6,12 +6,11 @@ using System.Linq.Expressions;
 
 namespace With.WithPlumbing
 {
-    using Collections;
     using Reflection;
 
     class ApplyOperation
     {
-        static Dictionary<string, Func<object, object[], object>> operations = new Dictionary<string, Func<object, object[], object>>()
+        static Dictionary<string, Func<object, object [], object>> operations = new Dictionary<string, Func<object, object [], object>> ()
         {
             { "Add", DoAdd },
             { "AddRange", DoAddRange },
@@ -19,81 +18,72 @@ namespace With.WithPlumbing
             { "Replace", DoReplace }
         };
 
-        public static object Apply(MethodCallExpression expr, object memberValue, object[] paramValues)
+        public static object Apply (MethodCallExpression expr, object memberValue, object [] paramValues)
         {
             var name = expr.Method.Name;
-            if (!operations.ContainsKey(name))
-            {
-                throw new ExpectedButGotException(operations.Keys.ToArray(),
+            if (!operations.ContainsKey (name)) {
+                throw new ExpectedButGotException (operations.Keys.ToArray (),
                     name);
             }
-            return operations[name](memberValue, paramValues);
+            return operations [name] (memberValue, paramValues);
         }
 
-        private static object DoReplace(object memberValue, object[] paramValues)
+        private static object DoReplace (object memberValue, object [] paramValues)
         {
             object value;
-            switch (paramValues.Length)
-            {
-                case 2:
-                    value = memberValue.ToDictionaryOfTypeT().Tap(d =>
-                    {
-                        d[paramValues[0]] = paramValues[1];
-                    }).ToDictionary();
-                    break;
-                default:
-                    throw new NotImplementedException();
+            switch (paramValues.Length) {
+            case 2:
+                value = memberValue.ToDictionaryOfTypeT ().Tap (d => {
+                    d [paramValues [0]] = paramValues [1];
+                });
+                break;
+            default:
+                throw new NotImplementedException ();
             }
             return value;
         }
 
-        private static object DoAdd(object memberValue, object[] paramValues)
+        private static object DoAdd (object memberValue, object [] paramValues)
         {
             object value;
-            switch (paramValues.Length)
-            {
-                case 1:
-                    value = ((IEnumerable)memberValue).ToListOfTypeT().Tap(l => 
-                        l.Add(paramValues[0]));
-                    break;
-                case 2:
-                    value = memberValue.ToDictionaryOfTypeT().Tap(d =>
-                        d.Add(paramValues[0], paramValues[1])
-                    ).ToDictionary();
-                    break;
-                default:
-                    throw new NotImplementedException();
+            switch (paramValues.Length) {
+            case 1:
+                value = ((IEnumerable)memberValue).ToListOfTypeT ().Tap (l =>
+                      l.Add (paramValues [0]));
+                break;
+            case 2:
+                value = memberValue.ToDictionaryOfTypeT ().Tap (d =>
+                      d.Add (paramValues [0], paramValues [1])
+                );
+                break;
+            default:
+                throw new NotImplementedException ();
             }
             return value;
         }
-        private static object DoAddRange(object memberValue, object[] paramValues)
+        private static object DoAddRange (object memberValue, object [] paramValues)
         {
-            var l = ((IEnumerable)memberValue).ToListOfTypeT();
-            foreach (var item in (IEnumerable)paramValues.Single())
-            {
-                l.Add(item);
+            var l = ((IEnumerable)memberValue).ToListOfTypeT ();
+            foreach (var item in (IEnumerable)paramValues.Single ()) {
+                l.Add (item);
             }
             return l;
         }
-        private static object DoRemove(object memberValue, object[] paramValues)
+        private static object DoRemove (object memberValue, object [] paramValues)
         {
-            switch (paramValues.Length)
-            {
-                case 1:
-                    if (memberValue.GetType().IsDictionaryType())
-                    {
-                        return memberValue.ToDictionaryOfTypeT().Tap(d =>
-                            d.Remove(paramValues[0])
-                        ).ToDictionary();
-                    }
-                    else
-                    {
-                        return ((IEnumerable)memberValue).ToListOfTypeT().Tap(l =>
-                            l.Remove(paramValues[0])
-                        );
-                    }
-                default:
-                    throw new NotImplementedException();
+            switch (paramValues.Length) {
+            case 1:
+                if (memberValue.GetType ().IsDictionaryType ()) {
+                    return memberValue.ToDictionaryOfTypeT ().Tap (d =>
+                          d.Remove (paramValues [0])
+                    );
+                } else {
+                    return ((IEnumerable)memberValue).ToListOfTypeT ().Tap (l =>
+                          l.Remove (paramValues [0])
+                    );
+                }
+            default:
+                throw new NotImplementedException ();
             }
         }
     }
