a53afb7c05ce4603d808e597b12cb6ed8a4c24b6
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2017-11-05 12:30:32 +0100

    Using benchmark dotnet (#33)
    * Using Benchmark Dot Net
    
    * Timings from both windows and Mac OS X

--- src/Timing/Program.cs
+++ src/Timing/Program.cs
@@ -2,67 +2,42 @@
 using With;
 using System.Linq;
 using System.Collections.Generic;
-#if MARKDOWNLOG
-using MarkdownLog;
-#endif
+using BenchmarkDotNet.Running;
+
 namespace Timing
 {
-    #if! MARKDOWNLOG
-    using System.Reflection;
-    static class Mardowns{
-        public static string ToMarkdownTable<T>(this IEnumerable<T> self){
-            var properties = typeof(T).GetProperties()
-                .Where(p=>p.CanRead && p.MemberType == MemberTypes.Property)
-                .ToArray();
-            var header = string.Join("|\t", properties.Select(p => p.Name).ToArray());
-            return header + "\n" + string.Join("\n", self.Select(v => MapProperties(properties, v)));
-        }
-        private static string MapProperties(PropertyInfo[] properties,Object v){
-            return String.Join("|\t", properties.Select(p=>p.GetValue(v,null)));
-        }
-    } 
-    #endif
 
     class MainClass
     {
 
-        public static void Main(string[] args)
+        public static void Main (string [] args)
         {
-            {
-                Console.WriteLine("Starting: Class with additional property");
-                var m = new TimingsNewClass();
-                var timings = m.Get().OrderBy(t => t.Value).ToArray();
-                var low = timings.Min(t => t.Value);
-                Console.WriteLine(timings.Select(t => Stat.Map(t, low)).ToMarkdownTable());
-            }
-            {
-                Console.WriteLine("Starting: Same Class");
-                var m = new TimingsClone();
-                var timings2 = m.Get().OrderBy(t => t.Value).ToArray();
-                var low = timings2.Min(t => t.Value);
-                Console.WriteLine(timings2.Select(t => Stat.Map(t, low)).ToMarkdownTable());
-            }
-            {
-                Console.WriteLine("Starting: Struct Equals");
-                var m = new TimingsEquals();
-                var timings2 = m.Get().OrderBy(t => t.Value).ToArray();
-                var low = timings2.Min(t => t.Value);
-                Console.WriteLine(timings2.Select(t => Stat.Map(t, low)).ToMarkdownTable());
+            switch (args.FirstOrDefault ()?.ToLowerInvariant ()) {
+            case "new": {
+                    Console.WriteLine ("Starting: Class with additional property");
+                    var summary = BenchmarkRunner.Run<TimingsNewClass> ();
+                    break;
+                }
+            case "clone": {
+                    Console.WriteLine ("Starting: Same Class");
+                    var summary = BenchmarkRunner.Run<TimingsClone> ();
+                    break;
+                }
+            case "equal": {
+                    Console.WriteLine ("Starting: Struct Equals");
+                    var summary = BenchmarkRunner.Run<TimingsEquals> ();
+                    break;
+                }
+            default: {
+                    Console.WriteLine (@"Select one of 
+new
+clone
+equal
+");
+                    break;
+                }
             }
         }
-            
-        class Stat
-        {
-            public static Stat Map(KeyValuePair<string,TimeSpan> method, TimeSpan low){
-                return new Stat
-                {   Name = String.Join(" ",method.Key.Split('_')),
-                    Elapsed = method.Value.ToString(), 
-                    Quotient = (method.Value.Ticks / low.Ticks).ToString()
-                };
-            }
-            public string Name { get; set;}
-            public string Elapsed { get; set;}
-            public string Quotient { get; set;}
-        }
+
     }
 }
