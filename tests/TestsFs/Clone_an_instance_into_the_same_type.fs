37dbbf8a00f59b985b28494d1faa814adff4d028
Oskar Gewalli <oskar@gewalli.se>
2018-05-10 14:35:11 +0200

    Renaming WithFs to TestFs

--- tests/TestsFs/Clone_an_instance_into_the_same_type.fs
+++ tests/TestsFs/Clone_an_instance_into_the_same_type.fs
@@ -9,23 +9,29 @@ type ``Clone an instance into the same type``()=
         Assert.Equal(newValue, ret.id)
         Assert.Equal(myClass.name, ret.name)
 
+    (*
     [<Theory; AutoData>]
     member this.``A class should be able to create a clone with a property set using equal`` (myClass:Customer, newValue:int)=
         let ret = myClass.With( fun m -> m.id = newValue)
         Assert.Equal(newValue, ret.id)
         Assert.Equal(myClass.name, ret.name)
+        *)
 
+    (*
     [<Theory; AutoData>]
     member this.``A class should be able to create a clone with two property set using equal`` (myClass:Customer, newIntValue:int, newStrValue:string)=
         let ret = myClass.With( fun m -> m.id = newIntValue && m.name = newStrValue)
         Assert.Equal(newIntValue, ret.id)
         Assert.Equal(newStrValue, ret.name)
+        *)
 
+    (*
     [<Theory; AutoData>]
     member this.``A class should be able to create a clone with a property set using equal and another propertyvalue``(anInstance:Customer, anotherInstance:Customer)=
         let ret = anotherInstance.With(fun m -> m.id = anInstance.id)
         Assert.Equal(anInstance.id, ret.id)
         Assert.Equal(anotherInstance.name, ret.name)
+        *)
 
     [<Theory; AutoData>]
     member this.``A class should be able to create a clone with a property set using eql`` (instance:Customer, newInt:int)=
