f02b2ff9ac155c54c259c86f4404d9a9b874375a
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2019-12-06 22:25:56 +0100

    Timing using language ext

--- src/Timing/Copy_update_single_property.cs
+++ src/Timing/Copy_update_single_property.cs
@@ -6,27 +6,11 @@ namespace Timing
 {
     public class Copy_update_single_property
     {
-        private static readonly MyClass _myClass = new MyClass(1, "2");
+        private static readonly Customer _myClass = new Customer(1, "2", new[] { "t" });
 
-        private static readonly IPreparedCopy<MyClass, string> _myClassPreparedCopy =
-            Prepare.Copy<MyClass, string>((m, v) => m.MyProperty2 == v);
+        private static readonly IPreparedCopy<Customer, string> _myClassPreparedCopy =
+            Prepare.Copy<Customer, string>((m, v) => m.Name == v);
 
-        public class MyClass
-        {
-            public MyClass(int myProperty, string myProperty2)
-            {
-                MyProperty = myProperty;
-                MyProperty2 = myProperty2;
-            }
-
-            public int MyProperty { get; private set; }
-            public string MyProperty2 { get; private set; }
-
-            internal MyClass SetMyProperty2(string v)
-            {
-                return new MyClass(MyProperty, v);
-            }
-        }
 
         [Benchmark]
         public void Using_static_prepered_copy_expression()
@@ -39,7 +23,13 @@ namespace Timing
         public void Hand_written_method_returning_new_instance()
         {
             var time = new DateTime(2001, 1, 1).AddMinutes(2);
-            var res = _myClass.SetMyProperty2(time.ToString());
+            var res = _myClass.SetName(time.ToString());
+        }
+        [Benchmark]
+        public void Language_ext_generated()
+        {
+            var time = new DateTime(2001, 1, 1).AddMinutes(2);
+            var res = _myClass.With(Name:time.ToString());
         }
     }
 }
--- src/Timing/Lens_with_three_properties.cs
+++ src/Timing/Lens_with_three_properties.cs
@@ -2,7 +2,6 @@ using System;
 using With;
 using System.Collections.Generic;
 using BenchmarkDotNet.Attributes;
-using With.Lenses;
 
 namespace Timing
 {
@@ -15,37 +14,7 @@ namespace Timing
         private static readonly IPreparedCopy<Customer, int, string, IEnumerable<string>> _usingHandWrittenLens =
             LensBuilder<Customer>.Of(Customer._IdHand).And(Customer._NameHand).And(Customer._PreferensesHand).BuildPreparedCopy();
         private static readonly IPreparedCopy<Customer, int, string, IEnumerable<string>> _usingHandWrittenCopyUpdate = new HandWrittenCopyUpdate();
-        public class Customer
-        {
-            public Customer(int id, string name, IEnumerable<string> preferences)
-            {
-                this.Id = id;
-                this.Name = name;
-                this.Preferences = preferences;
-            }
-            public int Id { get; }
-            public string Name { get; }
-            public IEnumerable<string> Preferences { get; }
-
-            internal Customer SetId(int v)
-            {
-                return new Customer(v, Name, Preferences);
-            }
-            internal Customer SetName(string v)
-            {
-                return new Customer(Id, v, Preferences);
-            }
-            internal Customer SetPreferences(IEnumerable<string> v)
-            {
-                return new Customer(Id, Name, v);
-            }
-            public static DataLens<Customer, int> _Id = LensBuilder<Customer>.Of(c => c.Id).Build();
-            public static DataLens<Customer, string> _Name = LensBuilder<Customer>.Of(c => c.Name).Build();
-            public static DataLens<Customer, IEnumerable<string>> _Preferenses = LensBuilder<Customer>.Of(c => c.Preferences).Build();
-            public static DataLens<Customer, int> _IdHand = DataLens<Customer, int>.Create(c=>c.Id,(c,v)=>c.SetId(v));
-            public static DataLens<Customer, string> _NameHand = DataLens<Customer, string>.Create(c => c.Name, (c, v) => c.SetName(v));
-            public static DataLens<Customer, IEnumerable<string>> _PreferensesHand = DataLens<Customer,IEnumerable<string>>.Create(c => c.Preferences, (c, v) => c.SetPreferences(v));
-        }
+     
         class HandWrittenCopyUpdate : IPreparedCopy<Customer, int, string, IEnumerable<string>>
         {
             public Customer Copy(Customer value1, int id, string name, IEnumerable<string> preferences)
@@ -69,5 +38,9 @@ namespace Timing
         {
             var res = _usingHandWrittenCopyUpdate.Copy(_myClass, 2, "2", new[] { "2" });
         }
+        /* [Benchmark]
+        public void Language_ext_lens()
+        {
+        } */
     }
 }
