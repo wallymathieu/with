b298cbc5f02986badb39d8f9719aa9925293703e
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2019-12-22 11:21:14 +0100

    -Collections

--- tests/Tests/Json/Json.cs
+++
@@ -1,9 +0,0 @@
-﻿using Newtonsoft.Json;
-namespace Tests.Json
-{
-    public static class Json
-    {
-        public static string ToJson(this object self)=>
-            JsonConvert.SerializeObject(self);
-    }
-}
